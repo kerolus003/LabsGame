684c576f562f336ce8541bfc6dd682fc91ccbafc
kero <kerlos245671@bue.edu.eg>
2025-10-28 11:29:14 +0300

    Lab 4

---
+++ labgame/Assets/CameraFollow.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+    public float Cameraspeed;
+    // Start is called before the first frame update
+    public float minX ,maxX;
+    public float minY ,maxY;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+void FixedUpdate()
+    {
+        if(Target!=null){
+        Vector2 newCamPosition = Vector2.Lerp(transform.position,Target.position,Time.deltaTime*Cameraspeed);
+        
+        float Clampx =Mathf.Clamp(newCamPosition.x,minX,maxX);
+        float Clampy =Mathf.Clamp(newCamPosition.y,minY,maxY);
+        transform.position=new Vector3(Clampx,Clampy,-10f);
+        }
+
+    }
+
+
+}
